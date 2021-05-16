94a4fe47734db69279136a7d79cff7c0307ab551
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-05-17 00:08:59 +0300

    created a like-button block

---
+++ #src/blocks/like-button/like.js
@@ -0,0 +1,23 @@
+(function() {
+  let likeBtns = document.querySelectorAll('.like');
+  likeBtns.forEach(likeBtn => {
+    initLike(likeBtn);
+    const likeCount = likeBtn.querySelector('.like__count span');
+    likeBtn.addEventListener('click', () => {
+      if (likeBtn.dataset.like == 'false') {
+        likeCount.textContent = +likeCount.textContent + 1;
+        likeBtn.dataset.like = 'true';
+      }
+      else {
+        likeCount.textContent = +likeCount.textContent - 1;
+        likeBtn.dataset.like = 'false';
+      }
+      likeBtn.classList.toggle('_active');
+    });
+  });
+
+  function initLike(likeBtn) {
+    if (likeBtn.dataset.like == 'true') likeBtn.classList.add('_active');
+    else likeBtn.classList.remove('_active');
+  }
+})();
